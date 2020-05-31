620d479fbcbcf0e91b02c12233dd4f3707cdf6b5
[name] <shashankshukla137@gmail.com>
2020-05-31 10:55:31 +0530

    Basic Structure Part-1

---
+++ app/src/main/java/com/example/shash/facts/CategoryFragment.kt
@@ -0,0 +1,39 @@
+package com.example.shash.facts
+
+import android.os.Bundle
+import android.view.*
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.shash.facts.databinding.FragmentCategoryBinding
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+class CategoryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentCategoryBinding>(
+            inflater,
+            R.layout.fragment_category, container, false
+        )
+
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, view!!.findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
+}
